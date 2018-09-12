bef54170df7e54fe9135a40af6a2343a01fde554
Татьяна Бондарева <tatanabondareva@MacBook-Air-Tatana.local>
2018-09-12 10:01:42 +0300

    *

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -4,8 +4,8 @@ export class List extends Component {
 	render() {
 		return <div className="app__form_container">
     				<div className="form_container__header form_container__header__violet">Лидеры</div>
-    					<div className={this.props.classNameOfText}>Нет участников</div>
         				<ul className="app__list">
+                            <li className={this.props.classNameOfText}>Нет участников</li>
         					{this.props.listOfMembers }
         				</ul>
         				<div className="app__toggles">{this.props.blockOfToggles}</div>
